bae8d20ae1fb47ecf14c8536e981bdf79cb21412
saidul-islam98 <saidulislam143.si@gmail.com>
2020-07-12 16:23:12 +0600

    parallax added for homepage

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 // import { Transition, TransitionGroup , CSSTransition } from 'react-transition-group';
 // import { play, exit } from './Transition/Transition';
+import { ParallaxProvider } from 'react-scroll-parallax';
 import Home from './Home/Home';
 import Footer from './Footer/Footer';
 import NavBar from './NavBar/NavBar';
@@ -12,36 +13,38 @@ import Contact from './Contact/Contact';
 class App extends Component {
   render() {
     return (
-      <Router basename="/">
-        <div className="app">
-          <NavBar />
-          {/* <Route render={({ location }) => {
-            const { pathname, key } = location; */}
+      <ParallaxProvider>
+        <Router basename="/">
+          <div className="app">
+            <NavBar />
+            {/* <Route render={({ location }) => {
+              const { pathname, key } = location; */}
 
-            {/* return ( */}
-              {/* // <TransitionGroup component={null}>
-              //   <CSSTransition key={location.key} in={true} classNames="slide-in">
-              //   {/* <Transition
-              //     key={key}
-              //     appear={true}
-              //     onEnter={(node, appears) => play(pathname, node, appears)}
-              //     onExit={(node, appears) => exit(node, appears)}
-              //     timeout={{enter: 750, exit: 150}}
-              //   > */}
-                  
-              <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/about" component={About}/>
-                <Route path="/contact" component={Contact}/>
-              </Switch>
-              {/* //   </CSSTransition>
-              // {/* //   </Transition> */}
-              {/* // </TransitionGroup>
-            )
-          }}/> */} 
-          <Footer/> 
-        </div>
-      </Router>
+              {/* return ( */}
+                {/* // <TransitionGroup component={null}>
+                //   <CSSTransition key={location.key} in={true} classNames="slide-in">
+                //   {/* <Transition
+                //     key={key}
+                //     appear={true}
+                //     onEnter={(node, appears) => play(pathname, node, appears)}
+                //     onExit={(node, appears) => exit(node, appears)}
+                //     timeout={{enter: 750, exit: 150}}
+                //   > */}
+                    
+                <Switch>
+                  <Route path="/" exact component={Home}/>
+                  <Route path="/about" component={About}/>
+                  <Route path="/contact" component={Contact}/>
+                </Switch>
+                {/* //   </CSSTransition>
+                // {/* //   </Transition> */}
+                {/* // </TransitionGroup>
+              )
+            }}/> */} 
+            <Footer/> 
+          </div>
+        </Router>
+      </ParallaxProvider>
     )
   }
 
--- src/components/backgroundtheme/Bgdtheme.js
+++ src/components/backgroundtheme/Bgdtheme.js
@@ -1,48 +1,60 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
-import bg4 from '../../assets/images/background-img/bg4.jpg';
+// import { Grid } from '@material-ui/core';
+import { motion } from 'framer-motion';
+// import { Parallax } from 'react-parallax';
+import { Parallax } from 'react-scroll-parallax';
+import bg4 from '../../assets/images/background-img/bg4rz.png';
 
 
 
 const useStyles = makeStyles((theme) => ({
     backgroundImg: {
         backgroundImage: `url(${bg4})`,
-        height: "33rem",
-        backgroundPosition: "center",
-        marginLeft: "1rem",
-        marginRight: "1rem",
+        height: "38rem",
+        backgroundAttachment: "contain",
+        // marginLeft: "1rem",
+        // marginRight: "1rem",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         position: "relative",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        color: "#000",  
-    },
+        // color: "#000",  
+    }, 
     desc: {
-        paddingTop: "1rem",
-        marginRight: "8.5rem",
+        padding: "0",
+        margin: "1rem 10rem 0rem 4rem",
+        // marginRight: "8.5rem",
         fontSize: "1rem",
-        lineHeight: "230%",
+        // lineHeight: "230%",
     }, 
     descSpnName: {
+        padding: "0",
+        margin: "0",
         fontSize: "2rem",
         fontWeight: "bold",
         color: "#203139",
-        fontFamily: "Lato",
+        fontFamily: "Roboto",
     },
     descSpn: {
+        padding: "0",
+        margin: "0",
         fontSize: "3rem",
         fontWeight: "bold",
         color: "#7A13F9",
         fontFamily: "Ubuntu",
     },
     descCur: {
-        paddingTop: "2rem",
-        fontFamily: "Lato",
+        padding: "1rem 0rem",
+        margin: "0",
+        fontFamily: "Roboto",
         fontSize: "1.3rem",
-        lineHeight: "130%",
+        // lineHeight: "130%",
+    },
+    boldf: {
+        fontWeight: "bold",
     }
 }));
   
@@ -52,22 +64,38 @@ const Bgdtheme = () => {
 
     return (
         <div>
-            <Grid container>
+            {/* <Grid container>
                 <Grid xs={12} sm={12} className={classes.backgroundImg}>
-                        <p className={classes.desc}>
+                        <motion.p className={classes.desc}
+                            initial={{ x:'-100vw' }}
+                            animate={{ x:0 }}
+                            transition={{ type: 'spring', delay: 0.5, stiffness: 50 }}>
                             <span className={classes.descSpnName}>Hello! I'm</span><br/> 
                             <span className={classes.descSpn}>Saidul</span><br/>
                             <span className={classes.descCur}>I am a <strong>Data Science</strong> and <strong>Machine Learning </strong>
                                  enthusiast. <br/> Currently working as a 
                                 part-time lecturer at a university.<br/></span>
-                        </p>
+                        </motion.p>
                 </Grid>
-            </Grid>
+            </Grid> */}
+
+            <Parallax 
+                y={[-5, 20]} tagOuter="figure"
+            >
+                {/* <img src={bg4} /> */}
+                <div className={classes.backgroundImg}>
+                    <motion.div className={classes.desc}
+                            initial={{ x:'-100vw' }}
+                            animate={{ x:0 }}
+                            transition={{ type: 'spring', delay: 0.5, stiffness: 50 }}>
+                        <h2 className={classes.descSpnName}>Hello! I'm</h2>
+                        <h1 className={classes.descSpn}>Saidul</h1>
+                        <p className={classes.descCur}>An avid learner, as well as a <span className={classes.boldf}>Data Science</span> & <br/> <span className={classes.boldf}>Machine Learning</span> enthusiast</p>
+                    </motion.div>
+                </div>
+            </Parallax>
         </div>
     )
 }
 
 export default Bgdtheme;
-
-
-
